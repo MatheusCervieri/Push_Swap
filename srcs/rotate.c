89d0b5401d64015ee439556bde184d9b5bfc544f
MatheusCervieri <mathcervieri@gmail.com>
2022-07-26 20:46:51 -0300

    Rotate working

--- srcs/rotate.c
+++ srcs/rotate.c
@@ -1,20 +1,17 @@
 #include "push_swap.h"
 
-
-
 void rotate(t_node **head)
 {
     t_node  *aux;
     t_node  *head_clone;
 
     head_clone = *head;
-    if(head_clone == NULL)
+    if(head_clone == NULL || head_clone->next == NULL)
         return ;
     aux = head_clone;
-    head_clone = head_clone->next;
+    *head = head_clone->next;
     if(head_clone)
         head_clone->prev = NULL;
     append_node(&head_clone, aux);
-    
-
+    ft_printf("ROTATE \n");
 }
