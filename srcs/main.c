90a56473473362617794168fb810a724a7e01a27
MatheusCervieri <mathcervieri@gmail.com>
2022-07-26 13:20:42 -0300

    Reciving but more organized

--- srcs/main.c
+++ srcs/main.c
@@ -1,7 +1,7 @@
 
 #include "push_swap.h"
 
-void input_validation(int argc, char *argv[], t_stacks *stacks)
+void input_validation(int argc, char *argv[], t_node *node)
 {
 	int	i;
 	if(argc < 2)
@@ -13,18 +13,19 @@ void input_validation(int argc, char *argv[], t_stacks *stacks)
 		// Lidar com exceções.
 		while(argv[i])
 		{ 
-			append_node(&stacks->head_a, new_node(ft_atoi(argv[i])));
+			append_node(&node, new_node(ft_atoi(argv[i])));
 			i++;
 		}
-		printn(stacks->head_a);
+		printn(node);
 	}
 }
 
 int main(int argc, char *argv[])
 {
-	t_stacks stacks;
-	stacks.head_a = NULL;
-
-	input_validation(argc, argv, &stacks);
+	//t_stacks stacks;
+//	stacks.head_a = NULL;
+	t_node	*stack_a;
+	stack_a = NULL;
+	input_validation(argc, argv, stack_a);
 	return (0);
 }
