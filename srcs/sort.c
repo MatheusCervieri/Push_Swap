3e6e493d5904720efac1f507897fcf2eab53ccb0
MatheusCervieri <mathcervieri@gmail.com>
2022-07-27 12:50:24 -0300

    Simple sort ok

--- srcs/sort.c
+++ srcs/sort.c
@@ -1,41 +1,17 @@
 #include "push_swap.h"
 
-/* 
-void	sort_three_elements_a_asc(t_stacks *stacks)
-{
-	int	first;
-	int	second;
-	int	third;
 
-	first = stacks->stack_a->content;
-	second = stacks->stack_a->next->content;
-	third = stacks->stack_a->next->next->content;
-	if (first > second && first > third && second > third)
-	{
-		swap_a(stacks);
-		reverse_rotate_a(stacks);
-	}
-	else if (first < second && first < third && second > third)
-	{
-		swap_a(stacks);
-		rotate_a(stacks);
-	}
-	else if (first > second && first < third && second < third)
-		swap_a(stacks);
-	else if (first < second && first > third && second > third)
-		reverse_rotate_a(stacks);
-	else if (first > second && first > third && second < third)
-		rotate_a(stacks);
-	return ;
-}
-*/
 
 /*
-    sa
+    sa:
     swap(stack_a, 'a');
+    rra:
+    reverse(stack_a, 'a');
+    ra:
+    rotate(stack_a, 'a');
 */
 
-void simple_sort(t_node **stack_a, t_node **stack_b)
+void simple_sort(t_node **stack_a)
 {
     int nb1;
     int nb2;
@@ -45,7 +21,20 @@ void simple_sort(t_node **stack_a, t_node **stack_b)
     nb3 = (*stack_a)->next->next->value;
     if(nb1 > nb2 && nb1 < nb3)
         swap(stack_a, 'a');
-    swap(stack_b, 'i');
+    if(nb1 > nb2 && nb1 > nb3 && nb2 > nb3)
+    {
+        swap(stack_a, 'a');
+        reverse(stack_a, 'a');
+    }
+    if(nb1 > nb2 && nb1 > nb3 && nb2 < nb3)
+        rotate(stack_a, 'a');
+    if(nb1 < nb2 && nb1 < nb3 && nb2 > nb3)
+    {
+        swap(stack_a, 'a');
+        rotate(stack_a, 'a');
+    }
+    if(nb1 < nb2 && nb1 > nb3 && nb2 > nb3)
+        reverse(stack_a, 'a');
 }
 
 void sort(t_node **stack_a, t_node **stack_b)
@@ -53,8 +42,8 @@ void sort(t_node **stack_a, t_node **stack_b)
     int size;
 
     size = list_size(*stack_a);
-    if(size >= 3)
-        simple_sort(stack_a, stack_b);
-
+    if(size == 3)
+        simple_sort(stack_a);
+    printn(*stack_b);
 }
 
