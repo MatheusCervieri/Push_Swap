106f391a5eca6d0cf0a6dbae2e1f79381c203d0b
MatheusCervieri <mathcervieri@gmail.com>
2022-08-01 22:39:59 -0300

    Norm fixing

--- srcs/is_sorted.c
+++ srcs/is_sorted.c
@@ -1,19 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   is_sorted.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mvieira- <mvieira-@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/01 22:38:47 by mvieira-          #+#    #+#             */
+/*   Updated: 2022/08/01 22:39:47 by mvieira-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
-int is_sorted(t_node **head)
+int	is_sorted(t_node **head)
 {
-    t_node  *aux;
-    t_node  *head_clone;
+	t_node	*aux;
+	t_node	*head_clone;
 
-    head_clone = *head;
-    if(!head_clone)
-        return (0);
-    aux = head_clone;
-    while(aux->next != NULL)
-    {
-        if(aux->value > aux->next->value)
-            return (0);
-        aux = aux->next;
-    }
-    return (1);
+	head_clone = *head;
+	if (!head_clone)
+		return (0);
+	aux = head_clone;
+	while (aux->next != NULL)
+	{
+		if (aux->value > aux->next->value)
+			return (0);
+		aux = aux->next;
+	}
+	return (1);
 }
