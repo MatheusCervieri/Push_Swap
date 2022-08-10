bc85706654985723fd933e81f02adb03ca6351a5
MatheusCervieri <mathcervieri@gmail.com>
2022-08-09 23:17:20 -0300

    find_mid.c norm fix

--- srcs/find_mid.c
+++ srcs/find_mid.c
@@ -6,7 +6,7 @@
 /*   By: mvieira- <mvieira-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 22:35:02 by mvieira-          #+#    #+#             */
-/*   Updated: 2022/08/09 10:47:05 by mvieira-         ###   ########.fr       */
+/*   Updated: 2022/08/09 23:15:39 by mvieira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,33 +33,12 @@ void	sort_array(int *array, int len)
 	}
 }
 
-int	find_mid(t_node *stack_a)
-{
-	int	*array_values;
-	int	stack_len;
-	int	i;
-
-	stack_len = list_size(stack_a);
-	array_values = malloc(stack_len * sizeof(int *));
-	if (!array_values)
-		ft_printf("COLOCAR UM ERRO AQUI");
-	i = 0;
-	while (i < stack_len)
-	{
-		array_values[i] = stack_a->value;
-		stack_a = stack_a->next;
-		i++;
-	}
-	sort_array(array_values, stack_len);
-	return (array_values[stack_len / 2]);
-}
-
 int	find_chunk(t_node *stack_a, int chunks_amount, int chunk)
 {
 	int	*array_values;
 	int	stack_len;
 	int	i;
-	int return_value;
+	int	return_value;
 
 	stack_len = list_size(stack_a);
 	array_values = malloc(stack_len * sizeof(int *));
