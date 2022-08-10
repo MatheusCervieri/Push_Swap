5f35de95644767ff200a075aacbb4370f024b461
MatheusCervieri <mathcervieri@gmail.com>
2022-08-10 00:17:24 -0300

    More norm fixing

--- srcs/hundreaded_sort.c
+++ srcs/hundreaded_sort.c
@@ -6,7 +6,7 @@
 /*   By: mvieira- <mvieira-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 23:02:55 by mvieira-          #+#    #+#             */
-/*   Updated: 2022/08/10 00:08:29 by mvieira-         ###   ########.fr       */
+/*   Updated: 2022/08/10 00:17:00 by mvieira-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,31 +42,31 @@ void	put_bigger_a(t_node **stack_b)
 	}
 }
 
-void	put_smaller_a(t_node **stack_b)
+void	put_smaller(t_node **head)
 {
 	int	size;
 	int	middle;
-	int	big_position;
+	int	small_position;
 	int	i;
 
-	size = list_size(*stack_b);
+	size = list_size(*head);
 	middle = size / 2;
-	big_position = get_node_position(*stack_b, get_min_node(*stack_b));
-	if (big_position > middle)
+	small_position = get_node_position(*head, get_min_node(*head));
+	if (small_position > middle)
 	{
 		i = size;
-		while (i + 1 > big_position)
+		while (i + 1 > small_position)
 		{
-			reverse(stack_b, 'a');
+			reverse(head, 'a');
 			i--;
 		}
 	}
 	else
 	{
 		i = 0;
-		while (i < big_position - 1)
+		while (i < small_position - 1)
 		{
-			rotate(stack_b, 'a');
+			rotate(head, 'a');
 			i++;
 		}
 	}
@@ -124,9 +124,9 @@ void	push_chunk_to_b(t_node **stack_a, t_node **stack_b, int big, int small)
 
 void	sort_five_test(t_node **stack_a, t_node **stack_b)
 {
-	put_smaller_a(stack_a);
+	put_smaller(stack_a);
 	push(stack_b, stack_a, 'b');
-	put_smaller_a(stack_a);
+	put_smaller(stack_a);
 	push(stack_b, stack_a, 'b');
 	simple_sort(stack_a);
 	insertion_sort(stack_a, stack_b);
@@ -150,13 +150,9 @@ void	hundread_sort(t_node **stack_a, t_node **stack_b)
 	free_list(&clone_stack);
 }
 
-void	super_sort(t_node **stack_a, t_node **stack_b)
+void	push_chunks_to_b(t_node *clone_stack, t_node **stack_a,
+	t_node **stack_b)
 {
-	t_node	*clone_stack;
-
-	clone_stack = clone_list(*stack_a);
-	push_chunk_to_b(stack_a, stack_b, find_chunk(clone_stack, 11, 1),
-		get_min_node(clone_stack));
 	push_chunk_to_b(stack_a, stack_b, find_chunk(clone_stack, 11, 2),
 		find_chunk(clone_stack, 11, 1));
 	push_chunk_to_b(stack_a, stack_b, find_chunk(clone_stack, 11, 3),
@@ -175,6 +171,16 @@ void	super_sort(t_node **stack_a, t_node **stack_b)
 		find_chunk(clone_stack, 11, 8));
 	push_chunk_to_b(stack_a, stack_b, find_chunk(clone_stack, 11, 10),
 		find_chunk(clone_stack, 11, 9));
+}
+
+void	super_sort(t_node **stack_a, t_node **stack_b)
+{
+	t_node	*clone_stack;
+
+	clone_stack = clone_list(*stack_a);
+	push_chunk_to_b(stack_a, stack_b, find_chunk(clone_stack, 11, 1),
+		get_min_node(clone_stack));
+	push_chunks_to_b(clone_stack, stack_a , stack_b);
 	push_chunk_to_b(stack_a, stack_b, get_max_node(clone_stack) + 1,
 		find_chunk(clone_stack, 11, 10));
 	insertion_sort(stack_a, stack_b);
